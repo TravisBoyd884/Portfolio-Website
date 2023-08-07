07405523d1fcaa6c2db14e2da2f4b11042250923
TravisBoyd884 <phtboyd884@gmail.com>
2023-08-06 20:27:09 -0400

    yelp

--- src/app/Anim.tsx
+++ src/app/Anim.tsx
@@ -1,81 +1,81 @@
-"use client";
-import { useCallback, useMemo, useRef } from "react";
-import { useFrame } from "@react-three/fiber";
-import * as THREE from "three";
-
-export default function Points() {
-  const loader = new THREE.TextureLoader();
-  const star = loader.load("/circle.png");
-  const bufferRef = useRef(null);
-
-  let t = 0;
-  let f = 0.002;
-  let a = 3;
-  const graph = useCallback(
-    (x: any, z: any) => {
-      return Math.sin(f * (x ** 2 + z ** 2 + t)) * a;
-    },
-    [t, f, a]
-  );
-
-  const count = 100;
-  const sep = 1.8;
-  let positions = useMemo(() => {
-    let positions = [];
-
-    for (let xi = 0; xi < count; xi++) {
-      for (let zi = 0; zi < count; zi++) {
-        let x = sep * (xi - count / 2);
-        let z = sep * (zi - count / 2);
-        let y = graph(x, z);
-        positions.push(x, y, z);
-      }
-    }
-
-    return new Float32Array(positions);
-  }, [count, sep, graph]);
-
-  useFrame(() => {
-    t += 15;
-
-    const positions = bufferRef.current.array;
-
-    let i = 0;
-    for (let xi = 0; xi < count; xi++) {
-      for (let zi = 0; zi < count; zi++) {
-        let x = sep * (xi - count / 2);
-        let z = sep * (zi - count / 2);
-
-        positions[i + 1] = graph(x, z);
-        i += 3;
-      }
-    }
-
-    bufferRef.current.needsUpdate = true;
-  });
-
-  return (
-    <points scale={0.2}>
-      <bufferGeometry attach="geometry">
-        <bufferAttribute
-          ref={bufferRef}
-          attach="attributes-position"
-          array={positions}
-          count={positions.length / 3}
-          itemSize={3}
-        />
-      </bufferGeometry>
-
-      <pointsMaterial
-        attach="material"
-        map={star}
-        color={0xffffff}
-        size={0.2}
-        sizeAttenuation
-        transparent={true}
-        alphaTest={0.5}
-        opacity={1.0}
-      />
-    </points>
-  );
-}
+/* "use client"; */
+/* import { useCallback, useMemo, useRef } from "react"; */
+/* import { useFrame } from "@react-three/fiber"; */
+/* import * as THREE from "three"; */
+/**/
+/* export default function Points() { */
+/*   const loader = new THREE.TextureLoader(); */
+/*   const star = loader.load("/circle.png"); */
+/*   const bufferRef = useRef(null); */
+/**/
+/*   let t = 0; */
+/*   let f = 0.002; */
+/*   let a = 3; */
+/*   const graph = useCallback( */
+/*     (x: any, z: any) => { */
+/*       return Math.sin(f * (x ** 2 + z ** 2 + t)) * a; */
+/*     }, */
+/*     [t, f, a] */
+/*   ); */
+/**/
+/*   const count = 100; */
+/*   const sep = 1.8; */
+/*   let positions = useMemo(() => { */
+/*     let positions = []; */
+/**/
+/*     for (let xi = 0; xi < count; xi++) { */
+/*       for (let zi = 0; zi < count; zi++) { */
+/*         let x = sep * (xi - count / 2); */
+/*         let z = sep * (zi - count / 2); */
+/*         let y = graph(x, z); */
+/*         positions.push(x, y, z); */
+/*       } */
+/*     } */
+/**/
+/*     return new Float32Array(positions); */
+/*   }, [count, sep, graph]); */
+/**/
+/*   useFrame(() => { */
+/*     t += 15; */
+/**/
+/*     const positions = bufferRef.current.array; */
+/**/
+/*     let i = 0; */
+/*     for (let xi = 0; xi < count; xi++) { */
+/*       for (let zi = 0; zi < count; zi++) { */
+/*         let x = sep * (xi - count / 2); */
+/*         let z = sep * (zi - count / 2); */
+/**/
+/*         positions[i + 1] = graph(x, z); */
+/*         i += 3; */
+/*       } */
+/*     } */
+/**/
+/*     bufferRef.current.needsUpdate = true; */
+/*   }); */
+/**/
+/*   return ( */
+/*     <points scale={0.2}> */
+/*       <bufferGeometry attach="geometry"> */
+/*         <bufferAttribute */
+/*           ref={bufferRef} */
+/*           attach="attributes-position" */
+/*           array={positions} */
+/*           count={positions.length / 3} */
+/*           itemSize={3} */
+/*         /> */
+/*       </bufferGeometry> */
+/**/
+/*       <pointsMaterial */
+/*         attach="material" */
+/*         map={star} */
+/*         color={0xffffff} */
+/*         size={0.2} */
+/*         sizeAttenuation */
+/*         transparent={true} */
+/*         alphaTest={0.5} */
+/*         opacity={1.0} */
+/*       /> */
+/*     </points> */
+/*   ); */
+/* } */
